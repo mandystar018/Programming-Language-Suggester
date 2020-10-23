ed9d528d4d7f8c6ec5b322a73c80ced9d0e6f693
Amanda Otero <mandystar018@gmail.com>
2020-10-23 14:26:05 -0700

    Add const userName in line 4, add .text() method after the user picks it answers and testing the first language JavaScrip to show in line 15-16 in the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,19 @@
-function findLanguage(input1, input2, input3, input4, input5){
-  let score = input1 + input2 + input3 + input4 + input5;
-
-  if (score <= 5) {
-    
-  } else if (score) {
-
-  } else if (score) {
-
-  } else {
-
-  }
-}
-
 $(document).ready(function(){
-  $("form#quiz").submit(function(event) {
+  $("form#form").submit(function(event) {
     event.preventDefault();
+    const userName = $("#person").val();
     const userInput1 = parseInt($("input:radio[name=question1]:checked").val());
     const userInput2 = parseInt($("input:radio[name=question2]:checked").val());
     const userInput3 = parseInt($("input:radio[name=question3]:checked").val());
     const userInput4 = parseInt($("input:radio[name=question4]:checked").val());
     const userInput5 = parseInt($("input:radio[name=question5]:checked").val());
-    $(".results").hide();
+    let score = userInput1 + userInput2 + userInput3 + userInput4 + userInput5;
+    
+    $("#userName").text(`${userName} your score is ${score} !.`)
+    $(".results div").hide();
 
-    findLanguage(input1, input2, input3, input4, input5);
+    if(score <= 5) {
+      $("#javaScript").show();
+    }
   });
 });
