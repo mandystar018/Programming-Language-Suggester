fb733ee7a8ec1194271c56a789a930f644f3d995
Amanda Otero <mandystar018@gmail.com>
2020-10-23 09:41:24 -0700

    Add form#quiz a submit listener, event.preventDefault method and 5 const saving the value of the first questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $("form#quiz").submit(function(event) {
+    event.preventDefaul();
+    const userInput1 = parseInt($("input:radio[name=question1]:checked").val());
+    const userInput2 = parseInt($("input:radio[name=question2]:checked").val());
+    const userInput3 = parseInt($("input:radio[name=question3]:checked").val());
+    const userInput4 = parseInt($("input:radio[name=question4]:checked").val());
+    const userInput5 = parseInt($("input:radio[name=question5]:checked").val());
+  });
 });
